d15c7f368e5570d6e2711b0d457d225cd84e7fc0
ohju3991 <ohju3991@student.otago.ac.nz>
2026-02-11 12:23:29 +1300

    organised dataset. starting the project.

--- project_sql/testing.sql
+++ project_sql/testing.sql
@@ -1,2 +1,16 @@
-SELECT *
-FROM orders;
+/*
+Data Analysis using E-Commerce Database
+1. Find the cities with the highest number of customers.
+2. Find the cities with the highest total orders.
+3. Find the most popular products.
+
+*/
+
+SELECT 
+    COUNT(customers.customer_id) AS total_customers,
+    customer_city AS city
+FROM customers
+INNER JOIN orders ON customers.customer_id = orders.customer_id
+GROUP BY customer_city
+ORDER BY total_customers DESC;
+
